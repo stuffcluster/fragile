1f609e5caf3df7447703ae65cf4a2a1452ded58f
spitchell <mr.spitchell@gmail.com>
2018-10-05 21:43:17 +0100

    OOP POC

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+class player {
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+    };
+    takeDamage(amount) {
+        this.health = this.health - amount;
+        console.log(this.health);
+    };
 
-let playerOne = 3;
-let playerTwo = 3;
-
-function hit(e) {
-    let result = Math.round(Math.random());
-    // let enemy = e.value === "player one" ? playerTwo : playerOne;
-    let enemy;
-    e.value === "playerOne" ? enemy = playerTwo : enemy = playerOne;
-    console.log(enemy);
-    // console.log(result > 0 ? "HIT" : "MISS");
-    result ? affect(enemy) : console.log("MISS");
 };
 
-function affect(enemy,result) {
-    enemy = enemy -1;
-    console.log("player one: " + playerOne);
-    console.log("player two: " + playerTwo);
+// the global variable has to be defined, not just declared 
+// using var instead of let or const so it can be inspected in dev tools
+var players = {};
 
+function init() {
+    players.playerOne = new player("Mark", 3);
+    players.playerTwo = new player("Lewis", 3);
+
+    document.querySelector("#playerOneStats").innerHTML = getStats(players.playerOne);
+    
+    console.table(players);
+};
+
+function getStats(player) { // should this be a method? a getter on the player object?
+  return (
+      `<h1>${player.name}</h1>`
+  ); 
 }
+
+window.onload = init(); 
+
