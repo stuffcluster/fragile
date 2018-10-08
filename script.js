b44cdbc01f85e107ab7932be67527fb3ec5a0b4a
spitchell <mr.spitchell@gmail.com>
2018-10-08 23:00:25 +0100

    lotsa options

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ class player {
         let playerInfo = (
             `<h1>${this.name}</h1>
             <div id="${this.name}-health" class="healthBar"></div>
-            <div id="${this.name}-might" class="mightBar"></div>
+            <div id="${this.name}-might" class="mightBar"><h1>${this.might}</h1></div>
             <h1>DEF: ${this.defense}</h1>
             <h1>ATT: ${this.attack}</h1>
             <h1>MOD: ${this.modifier}</h1>
@@ -72,7 +72,7 @@ class player {
         let heartBar = document.querySelector(`#${this.name}-health`);
         for(let i=0; i<this.health; i++){
             let heartNode = document.createElement("div");
-            heartNode.classList.add("mightIcons");
+            heartNode.classList.add("heartIcon");
             heartNode.innerHTML = heart;
             heartBar.appendChild(heartNode);
         }   
@@ -80,12 +80,12 @@ class player {
         <path d="M47.923 29.694c.021-.601-.516-1.063-.901-1.515-.676-2.733-2.016-5.864-3.961-8.971C39.942 14.23 31.688 6.204 28.553 4.966a2.797 2.797 0 0 0-.429-.126c-.313-1.013-.479-1.708-1.698-2.521C23.072.083 19.327-.547 16.848.476c-2.481 1.023-3.859 6.687-1.19 8.625 2.546 1.857 7.583-1.888 9.195.509 1.609 2.396 3.386 10.374 6.338 15.473a17 17 0 0 0-2.307-.156c-3.406 0-6.467.998-8.63 2.593-1.85-2.887-5.08-4.806-8.764-4.806-3.82 0-7.141 2.064-8.95 5.13v22.619h4.879l1.042-1.849c3.354-1.287 7.32-4.607 10.076-8.147 11.014 4.322 29.139-3.678 29.386-10.773z" fill="#010002"/>
       </svg>`;
         let mightBar = document.querySelector(`#${this.name}-might`);
-        for(let i=0; i<this.might; i++){
-            let mightNode = document.createElement("div");
-            mightNode.classList.add("mightIcons");
-            mightNode.innerHTML = arm;
-            mightBar.appendChild(mightNode);
-        } 
+   
+        let mightNode = document.createElement("div");
+        mightNode.classList.add("mightIcon");
+        mightNode.innerHTML = arm;
+        mightBar.appendChild(mightNode);
+
     }  
 };
 
