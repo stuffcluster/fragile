118c8847d9c1fb26d9e71c6e4734e1a11079ebc3
spitchell <mr.spitchell@gmail.com>
2018-10-09 11:32:10 +0100

    defence value added to shield icon

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ class player {
     let playerInfo = (
       `<h1>${this.name}</h1>
       <div id="${this.name}-health" class="healthBar"></div>
-      <div id="${this.name}-might" class="mightBar"><h1>${this.might}</h1></div>
-      <div id="${this.name}-shield"><h1>${this.defense}</h1><div>
+      <div id="${this.name}-might" class="mightBar"></div>
+      <div id="${this.name}-shield"><div>
       <h1>ATT: ${this.attack}</h1>
       <h1>MOD: ${this.modifier}</h1>
       `
@@ -78,7 +78,7 @@ class player {
     }   
     let arm = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.463 50.463">
     <path d="M47.923 29.694c.021-.601-.516-1.063-.901-1.515-.676-2.733-2.016-5.864-3.961-8.971C39.942 14.23 31.688 6.204 28.553 4.966a2.797 2.797 0 0 0-.429-.126c-.313-1.013-.479-1.708-1.698-2.521C23.072.083 19.327-.547 16.848.476c-2.481 1.023-3.859 6.687-1.19 8.625 2.546 1.857 7.583-1.888 9.195.509 1.609 2.396 3.386 10.374 6.338 15.473a17 17 0 0 0-2.307-.156c-3.406 0-6.467.998-8.63 2.593-1.85-2.887-5.08-4.806-8.764-4.806-3.82 0-7.141 2.064-8.95 5.13v22.619h4.879l1.042-1.849c3.354-1.287 7.32-4.607 10.076-8.147 11.014 4.322 29.139-3.678 29.386-10.773z" fill="#010002"/>
-    <text x="10" y="35" class="might-text">${this.might}</text>
+    <text x="40%" y="70%" class="might-text">${this.might}</text>
     </svg>`;
     let mightBar = document.querySelector(`#${this.name}-might`);
         let mightNode = document.createElement("div");
@@ -86,8 +86,9 @@ class player {
     mightNode.innerHTML = arm;
     mightBar.appendChild(mightNode);
 
-    let shield = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 352.644 352.644">
+    let shield = `<svg xmlns="http://www.w3.org/2000/svg"  width="50%" height=" 50%" viewBox="0 0 352.644 352.644">
     <path d="M324.478 51.943L177.986.285a4.994 4.994 0 0 0-3.326 0L28.166 51.943a5 5 0 0 0-3.337 4.715c0 52.278 13.834 112.711 37.956 165.805 19.566 43.069 54.751 100.519 111.248 129.625a5 5 0 0 0 4.58 0c56.496-29.106 91.68-86.556 111.247-129.625 24.121-53.094 37.955-113.527 37.955-165.805a5 5 0 0 0-3.337-4.715z"/>
+    <text x="40%" y="50%" class="shield-text">${this.defense}</text>
   </svg>
   `;
     let shieldBar = document.querySelector(`#${this.name}-shield`);
