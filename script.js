38b5758dfbefe4e176cd207d076032c8aee9aebe
spitchell <mr.spitchell@gmail.com>
2018-10-17 16:35:50 +0100

    specific classnames added

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ const hero = new Entity(heroStats);
 const monster = new Entity(monsterStats);
 const battle = new Fight(hero, monster);
 
+function init() {
+  const display = document.querySelector(".display");
+
+  const playerOneBoard = document.createElement("div");
+  playerOneBoard.className = battle.fighters[0].name;
+  display.appendChild(playerOneBoard);
+
+  const playerTwoBoard = document.createElement("div");
+  playerTwoBoard.className = battle.fighters[1].name;
+  display.appendChild(playerTwoBoard);
+
+};
 const renderPlayer = (entity) => {
-  let playerOneBoard = document.querySelector(".player1")
+  let playerOneBoard = document.querySelector(".playerOne");
+  let playerTwoBoard = document.querySelector(".playerTwo");
   let stats = `
-    HP: ${entity.health}
+    <p>HP: ${entity.health}</p>
+    <p>AT: ${entity.attack}</p>
+    <p>DF: ${entity.defend}</p>
+    <p>SP: ${entity.speed}</p>
+    <p>ST: ${entity.stamina}</p>
   `;
   playerOneBoard.innerHTML = stats;
 }
@@ -21,3 +38,4 @@ window.hero = hero;
 window.GetStats = GetStats;
 window.monster = monster;
 window.renderPlayer = renderPlayer;
+window.init = init;
